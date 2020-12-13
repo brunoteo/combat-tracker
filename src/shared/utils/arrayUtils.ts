914bb29ae402d6157fe4a2ffd56d6ea86cd394d6
Andrea Conforto <andrea@Andreas-MBP.lan>
2020-12-14 00:13:01 +0100

    revert to 158b15ce55c8da85dd11fc7f4be06c0f8e44073c

--- src/shared/utils/arrayUtils.ts
+++
@@ -1,7 +0,0 @@
-export const findIndexToInsert = <T>(elementToCompare: T, decrescentSortedArray: T[], extractor: (p: T) => number): number => {
-    for(let i: number = 0; i<decrescentSortedArray.length; i++)
-        if(extractor(decrescentSortedArray[i]) < extractor(elementToCompare))
-            return i;
-
-    return decrescentSortedArray.length;
-}
